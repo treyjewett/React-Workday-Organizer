88c6ce69c33980626ed9eb82c5dec5e5291cb17c
Russell Jewett <rfjewett3@gmail.com>
2021-03-07 16:55:38 -0500

    local storage working, adds moment.js component to header. hour blocks still need styling.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Moment from 'react-moment';
+import 'moment-timezone';
 import "./style.css";
 
 function Header() {
@@ -7,7 +9,7 @@ function Header() {
             <header className="jumbotron">
                 <h1 className="display-3">Work Day Scheduler</h1>
                 <p className="lead">A simple calendar app for scheduling your work day</p>
-                <p id="currentDay" className="lead"></p>
+                <p id="currentDay" className="lead"><Moment format="dddd, MMMM Do YYYY, h:mma" /></p>
             </header>
     )
 };
